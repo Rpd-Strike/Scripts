2a0cc140af6cf12e3fc2d041086a46af815e694b
Felix Puscasu <puscasu.felix1@gmail.com>
2023-01-08 11:22:07 +0200

    h

--- Linux/programs/conda/conda_activate.sh
+++ Linux/programs/conda/conda_activate.sh
@@ -1,6 +1,6 @@
 conda_activate_for_scripts() {
   # Workaround for a conda bug: https://github.com/conda/conda/issues/7980
-  eval "$(conda shell.`basename -- $SHELL` hook)"
+  eval "$(${CONDA_INSTALL_PREFIX}/bin/conda shell.`basename -- $SHELL` hook)"
   conda activate $1  # Will activate the base conda env if no arguments are passed.
   echo "Active conda env for process $$ is now: ${CONDA_DEFAULT_ENV} at ${CONDA_PREFIX}"
 }
