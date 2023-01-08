5f8b27823a1ca1e923e17c08e6a37cbe99c34916
Felix Puscasu <puscasu.felix1@gmail.com>
2023-01-08 13:10:48 +0200

    ugh

--- Linux/programs/ohmyzsh/install_ohmyzsh.sh
+++ Linux/programs/ohmyzsh/install_ohmyzsh.sh
@@ -4,12 +4,21 @@ mkdir -p $HOME/apps/ohmyzsh
 
 echo "ZSH variable: $ZSH"
 
-rm -rf $HOME/.oh-my-zsh
+# Prepping in case of previous oh my zsh installs
+FILE=$HOME/.zshrc.pre-oh-my-zsh
+if test -f "$FILE"; then
+    echo "$FILE exists... renaming"
+    mv $FILE $FILE.old
+fi
 
-cd $HOME/apps/ohmyzsh && ZSH=$HOME/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+FOLDER=$HOME/.oh-my-zsh
+if [ -d "$FOLDER" ]; then
+    echo "$FOLDER exists... deleting"
+    rm -rf $FOLDER
+fi
+
+# Do install
+ZSH=$HOME/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cat $HOME/.zshrc.pre-oh-my-zsh >> ~/.zshrc
-
-cd $OLDPWD
-
 
