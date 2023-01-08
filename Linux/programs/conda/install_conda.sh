bd785f6277342c919e8934094e33be2b5431d078
Felix Puscasu <puscasu.felix1@gmail.com>
2023-01-08 10:18:39 +0200

    hey

--- Linux/programs/conda/install_conda.sh
+++ Linux/programs/conda/install_conda.sh
@@ -13,7 +13,7 @@ function _install_miniconda() {
     ${CONDA_INSTALLER_PATH} -b -p ${CONDA_INSTALL_PREFIX}
     echo " --> Finish installing"
     ${CONDA_INSTALL_PREFIX}/bin/activate base
-    ${CONDA_INSTALL_PREFIX}/bin/conda upgrade -y --all -c -conda-forge
+    ${CONDA_INSTALL_PREFIX}/bin/conda upgrade -y --all -c conda-forge
     ${CONDA_INSTALL_PREFIX}/bin/conda info
     ${CONDA_INSTALL_PREFIX}/bin/conda clean -ya
     echo " --> installing base tools"
